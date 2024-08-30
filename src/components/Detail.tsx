d6cc713eee8ee61de91654af1e6fcf6142464676
_pooh <natsathon2018@gmail.com>
2024-08-31 06:29:04 +0700

    commit

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,8 +1,10 @@
 import { ThemeContext } from "@contexts/ThemeContext";
+import { LangContext } from "@contexts/LangContext";
 import { useContext } from "react";
 
 export const Detail = () => {
   const { theme } = useContext(ThemeContext);
+  const { lang } = useContext(LangContext);
   return (
     <div>
       <p className="text-center" style={{ color: theme.fgColor }}>
